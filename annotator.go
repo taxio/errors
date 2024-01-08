b7a694c2d05bebf5834c5479ac72f45437dd3eed
Takuma Shiotsu <taxio@users.noreply.github.com>
2024-01-08 16:52:52 +0900

    must attach callstack (#3)

--- annotator.go
+++ annotator.go
@@ -1,25 +1,10 @@
 package errors
 
-import "runtime"
-
 type Annotator func(*Error)
-
-func callers() *stack {
-	var pcs [32]uintptr
-	n := runtime.Callers(3, pcs[:])
-	var st stack = pcs[0:n]
-	return &st
-}
 
 func WithMessage(message string) Annotator {
 	return func(err *Error) {
 		err.message = message
-	}
-}
-
-func WithStack() Annotator {
-	return func(err *Error) {
-		err.stack = callers()
 	}
 }
 
